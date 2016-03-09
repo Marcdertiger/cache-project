8ced4ae845d04d5d0bf61815ffbb92b1b5825759
Marc <marcou1750@gmail.com>
2016-03-08 23:22:36 -0400

    added 3 vhd files that will make up the cache, and a directory where to store references used

---
+++ cache-system/SRAM.vhd
@@ -0,0 +1,5 @@
+-- SRAM
+
+-- contains the data lines of the cache
+
+-- 8 lined of 4 words, words of 16 bits
---
+++ cache-system/TRAM.vhd
@@ -0,0 +1,7 @@
+-- TRAM 
+
+-- Memory where the tag of each line is stored
+
+-- Has a two way port to the Cache Controller (read and write) ********** 
+
+-- 8 tag lenght (to match number of lines in cache) 
